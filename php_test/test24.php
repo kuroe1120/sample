7ac3db76f9572ad29488f4312982256a7d974e92
kuroe1120 <ryusei131216@gmail.com>
2023-02-22 22:39:14 +0000

    24test

---
+++ php_test/test24.php
@@ -0,0 +1,44 @@
+<?php
+// 課題1
+
+$a = 3;
+$b = 7;
+$ab = $a + $b;
+echo $ab;
+
+// 課題2
+
+$array_month = ["１月", "２月","３月" ,"４月" ,"５月" ,"６月" ,"７月" ,"８月" ,"９月" ,"１０月" ,"１１月" ,"１２月"];
+echo $array_month[9];
+
+// 課題3
+
+$hello = "Hello";
+$name = "ryusei ogami";
+$world = "s World";
+$hnw = $hello + $name + $world;
+echo $hnw;
+
+//課題4
+
+$tech_boost = "tech";
+$tech_boost .= " boost";
+echo $tech_boost;
+
+//課題5
+
+$2018_calendar = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+echo $2018_calendar["December"];
