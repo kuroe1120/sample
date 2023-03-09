ccf93e02cf20588d48ce58ee06352e40b6a54f7d
kuroe1120 <kuroe0720@i.softbank.jp>
2023-03-09 08:57:27 +0000

    課題47~50

---
+++ javascript_test/javatest2.js
@@ -0,0 +1,4 @@
+let price = prompt();
+
+let x = price * 1.1;
+alert(x);
---
+++ javascript_test/javatest3.js
@@ -0,0 +1,41 @@
+let mon = "February";
+
+if (mon === "January") {
+    console.log("January");
+    
+} else if (mon === "February") {
+    console.log("February");
+    
+} else if (mon === "March") {
+    console.log("March");
+    
+} else if (mon === "April") {
+    console.log("April");
+    
+} else if (mon === "May") {
+    console.log("May");
+    
+} else if (mon === "June") {
+    console.log("June");
+    
+} else if (mon === "July") {
+    console.log("July");
+    
+} else if (mon === "August") {
+    console.log("August");
+    
+} else if (mon === "September") {
+    console.log("September");
+    
+} else if (mon === "October") {
+    console.log("October");
+    
+} else if (mon === "November") {
+    console.log("November");
+    
+} else if (mon === "December") {
+    console.log("December");
+    
+} else {
+    console.log("不明です");
+}
