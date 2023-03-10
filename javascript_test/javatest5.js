8ed7ac3bab3c15d8a0a10ae4575f3a8e517a7bed
kuroe1120 <kuroe0720@i.softbank.jp>
2023-03-10 03:12:03 +0000

    課題50~51

---
+++ javascript_test/javatest5.js
@@ -0,0 +1,9 @@
+//課題１
+let fruit = [ "apricot", "strawberry" ,"apple", "orange"];
+//課題２
+fruit.unshift("pear");
+//課題３
+fruit.push("grape");
+//課題４,5
+fruit[5] = "lime"
+console.log(fruit);
---
+++ javascript_test/javatest6.js
@@ -0,0 +1,8 @@
+//課題１,２
+let fruit = {apple:"りんご", strawberry:"いちご", grape:"ぶどう", lemon:"れもん"};
+console.log(fruit["grape"]);
+//課題３
+let key = Object.keys(fruit);
+console.log(key);
+//課題４
+console.log(fruit);
